6b09171ff9db227e2853a9c97ad35f363af45ea0
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-06-27 16:55:32 +0800

    created the class for the tetriminos and blocks

---
+++ tetrimino.py
@@ -0,0 +1,9 @@
+from settings import *
+from blocks import Block
+
+class Tetrimino:
+    def __init__(self, tetris):
+        self.tetris = tetris
+
+    def update(self):
+        pass
--- tetris.py
+++ tetris.py
@@ -1,9 +1,12 @@
 from settings import *
 import math
+from tetrimino import Tetrimino
 
 class Tetris:
     def __init__(self,app):
         self.app = app
+        self.sprite_group = pg.sprite.Group()
+        self.tetrimino = Tetrimino(self)
 
     def draw_grid(self):
         for x in range(field_w):
@@ -12,7 +15,9 @@ class Tetris:
                              (x * tile_size, y * tile_size, tile_size, tile_size), 1)
 
     def update(self):
-        pass
+        self.tetrimino.update()
+        self.sprite_group.update
 
     def draw(self):
         self.draw_grid()    
+        self.sprite_group.draw(self.app.screen)
