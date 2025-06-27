375ad985f732e292675d3604f712973f569839d0
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-06-27 17:04:03 +0800

    as a test, a block now has been placed on the board

--- blocks.py
+++ blocks.py
@@ -1,4 +1,12 @@
 from settings import *
 
-class Block:
-    pass
+class Block (pg.sprite.Sprite):
+    def __init__(self, tetrimino, pos):
+        self.tetromino = tetrimino
+
+        super().__init__(tetrimino.tetris.sprite_group)
+        self.image = pg.Surface([tile_size,tile_size])
+        self.image.fill('orange')
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos[0] * tile_size, pos[1] * tile_size
--- tetrimino.py
+++ tetrimino.py
@@ -4,6 +4,7 @@ from blocks import Block
 class Tetrimino:
     def __init__(self, tetris):
         self.tetris = tetris
+        Block(self, (4,7))
 
     def update(self):
         pass
